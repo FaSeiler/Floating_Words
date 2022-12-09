d973a109e2c3899831b0fa3232fcd9be16334ecd
Coelacanth <ga86jab@mytum.de>
2022-12-09 17:56:05 +0100

    fix showinfo.cs errors

--- Assets/Scripts/ShowInfo.cs
+++ Assets/Scripts/ShowInfo.cs
@@ -6,16 +6,14 @@ using UnityEngine.UI;
 using System;
 using UnityEngine.Networking;
 using SimpleJSON;
-using System;
-using UnityEngine.Networking;
-using SimpleJSON;
-    public GameObject labelParentGO;
+
 public class ShowInfo : MonoBehaviour
-    public GameObject DropdownPrefab;
-
+{
+    public Canvas canv;
     public GameObject Textprefab;
-    public GameObject DropdownPrefab;
-
+    
+    public void ShowLabel(string label,Vector2 center)
+    {
         TranslateText("en","de", label, (success, translatedText) =>
         {
             if (success)
@@ -23,63 +21,22 @@ public class ShowInfo : MonoBehaviour
                 Debug.Log(translatedText);
 
                 GameObject newText= Instantiate(Textprefab, new Vector2(0,0), Quaternion.identity);
-                GameObject dropdown_menu = Instantiate(DropdownPrefab, new Vector2(0,0), Quaternion.identity);
 
                 //TODO : optimize the code
                 newText.transform.SetParent(canv.gameObject.transform);
-
-                dropdown_menu.transform.SetParent(canv.gameObject.transform);
-
                 newText.transform.localScale = new Vector3(
                 newText.transform.localScale.x * canv.gameObject.transform.localScale.x,
                 newText.transform.localScale.y * canv.gameObject.transform.localScale.y,
                 newText.transform.localScale.z * canv.gameObject.transform.localScale.z);
-                newText.transform.localScale.x * canv.gameObject.transform.localScale.x,
+
                 newText.transform.position = center;
-                dropdown_menu.transform.position = center;
+                //newText.GetComponent<TMP_Text>().text=label;
                 newText.GetComponent<TMP_Text>().text=translatedText;
             }
         });
 
 
     }
-    
-
-    public void TranslateText(string sourceLanguage, string targetLanguage, string sourceText, Action<bool, string> callback)
-    {
-        StartCoroutine(TranslateTextRoutine(sourceLanguage, targetLanguage, sourceText, callback));
-    }
-    
-    private static IEnumerator TranslateTextRoutine(string sourceLanguage, string targetLanguage, string sourceText, Action<bool, string> callback)
-    {
-        var url = $"https://translate.googleapis.com/translate_a/single?client=gtx&sl={sourceLanguage}&tl={targetLanguage}&dt=t&q={UnityWebRequest.EscapeURL(sourceText)}";
-
-        var webRequest = UnityWebRequest.Get(url);
-
-        yield return webRequest.SendWebRequest();
-
-        if (webRequest.isHttpError || webRequest.isNetworkError)
-        {
-            Debug.LogError(webRequest.error);
-            callback.Invoke(false, string.Empty);
-
-            yield break;
-        }
-
-        var parsedTexts = JSONNode.Parse(webRequest.downloadHandler.text);
-        var translatedText = parsedTexts[0][0][0];
-
-        callback.Invoke(true, translatedText);
-
-
-
-                newText.GetComponent<TMP_Text>().text=translatedText;
-            }
-        foreach (Transform child in labelParentGO.transform)
-
-
-    }
-    
 
     public void TranslateText(string sourceLanguage, string targetLanguage, string sourceText, Action<bool, string> callback)
     {
@@ -111,7 +68,7 @@ public class ShowInfo : MonoBehaviour
 
     public void CleanAll()
     {
-        foreach (Transform child in labelParentGO.transform)
+        foreach (Transform child in canv.transform)
         {
             GameObject.Destroy(child.gameObject);
         }
